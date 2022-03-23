5e4f156698c8de3ab349e364e1d4b722233b0951
uikoo9 <uikoo9@qq.com>
2022-03-23 19:43:08 +0800

    feat: add login title

--- src/components/login/login-ui/login.js
+++ src/components/login/login-ui/login.js
@@ -4,6 +4,8 @@
 import React from 'react';
 
 // login
+import 'normalize.css';
+import './login.scss';
 import LoginInput from './login-input.js';
 import LoginBtn from './login-btn.js';
 
@@ -14,6 +16,7 @@ export default class LoginComponent extends React.Component {
   render() {
     return (
       <div className='login-container'>
+          <div className='login-title'></div>
           <LoginInput type='text' onChange={this.props.onNameChange}/>
           <LoginInput type='password' onChange={this.props.onPwdChange}/>
           <LoginBtn onClick={this.props.onClick} name='login'/>
