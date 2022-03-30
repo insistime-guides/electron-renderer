894eb2edd7a877660339dc2ee2bb51d885d9152a
uikoo9 <uikoo9@qq.com>
2022-03-30 19:44:11 +0800

    feat: open url

--- src/components/index/index-container.js
+++ src/components/index/index-container.js
@@ -7,9 +7,18 @@ import React from 'react';
  * index container
  */
 export default class IndexContainer extends React.Component {
+
+  onClick(){
+    const url = 'https://www.baidu.com/';
+    window.electron.openUrl(url);
+  }
+
   render() {
     return (
-      <p>hello world</p>
+      <>
+        <p>hello world</p>
+        <button onClick={this.onClick}>click</button>
+      </>
     );
   }
 }
