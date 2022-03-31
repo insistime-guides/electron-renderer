d6a598df2367d7100b8cc0ec3f7d0c326b7ce1e1
uikoo9 <uikoo9@qq.com>
2022-03-31 08:53:06 +0800

    feat: log init

--- src/components/index/index-container.js
+++ src/components/index/index-container.js
@@ -3,6 +3,9 @@
 // react
 import React from 'react';
 
+// log
+import { infoLog } from '@util/log.js';
+
 /**
  * index container
  */
@@ -15,6 +18,8 @@ export default class IndexContainer extends React.Component {
     await window.electron.lsSet('test', 'nihao');
     let s = await window.electron.lsGet('test');
     console.log(s);
+
+    infoLog('nihao nihao');
   }
 
   render() {
